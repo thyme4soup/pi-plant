fe7356298813d01359d8ed9c4cfca5ac51cc5c5c
thyme4soup-pi <mchenevey7@gmail.com>
2019-09-11 20:49:26 +0100

    edits from pi

--- scheduler.sh
+++ scheduler.sh
@@ -1,2 +1,3 @@
-/usr/bin/python3 /Users/michael/Documents/pi-plant/sensor.py
-/usr/bin/python3 /Users/michael/Documents/pi-plant/water.py auto
+/usr/bin/python3 /home/pi/pi-plant/sensor.py
+/usr/bin/python3 /home/pi/pi-plant/water.py auto
+
--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+cd /home/pi/pi-plant/ && /usr/bin/git pull && /usr/bin/pip3 install -r requirements.txt
 /usr/bin/python3 /home/pi/pi-plant/app.py &
