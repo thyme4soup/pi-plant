b0566e92f7d17dfc1792d27d2ad9f1a1422e65a4
Michael Chenevey <michael@Michaels-MacBook-Air-4.local>
2020-02-26 18:12:12 -0500

    that didnt work

--- web/app.py
+++ web/app.py
@@ -123,4 +123,4 @@ def video_feed():
     return send_from_directory('resources/', 'plantgif.gif')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True, use_reloader=True, debug=True)
+    app.run(host='0.0.0.0', threaded=True, use_reloader=True, debug=True)
